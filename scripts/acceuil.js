b8855f3e0323978369b3232d659edb1cc2311be9
Antoine Bigot <62606118+NekShor@users.noreply.github.com>
2025-06-27 16:07:27 +0200

    item screen acceuil

---
+++ scripts/acceuil.js
@@ -0,0 +1,41 @@
+var slot_size = "30px";
+
+function get_one_item () {
+    var randomIndex = Math.floor(Math.random() * items.length);
+    var item = items[randomIndex];
+    return item;
+}
+
+function lauch_items () {
+    set_item_to_page();
+    setInterval(function() {
+        set_item_to_page();
+    }, 3000);
+}
+
+function set_item_to_page(){
+    var new_item = get_one_item();
+    var div = document.createElement("div");
+    div.className = "back-items invslot items_inv";
+    div.innerHTML = `<img src="items/texture/${new_item.code}.png">`;
+    div.setAttribute('onClick', `do_sound('drop')`);
+
+    var top_position = Math.floor(Math.random() * (window.innerHeight - parseInt(slot_size)));
+    div.style.top = top_position + 'px';
+
+    document.body.appendChild(div);
+    setTimeout(function() {
+        div.style.left = '100%';
+    }, 100);
+
+    setTimeout(function() {
+        if (div.parentNode) {
+            div.parentNode.removeChild(div);
+        }
+    }, 30000);
+}
+
+function do_sound (sound) {
+    var audio = new Audio(`sounds/${sound}.mp3`);
+    audio.play();
+}
